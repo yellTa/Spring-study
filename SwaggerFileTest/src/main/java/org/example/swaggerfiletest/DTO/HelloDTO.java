2b4f35f50df50009c08628867ca105d4e40740b4
YellTa <>
2025-01-14 15:05:07 +0900

    feat(파일 업로드) : ModelAttribute를 활용한 fileUpload
    FileController.java=====
    ModelAttribute를 활용해서 데이터를 받아서 활용
    Response message에 파일이름, 사이즈, string 메시지 출력
    
    HelloDTO.java=====
    DTO 설정
    Setter사용하지 않아 제거
    
    modelAttribute를 사용한 경우에도 Setter를 사용하지 않고 fileUpload swagger로 확인 가능

--- SwaggerFileTest/src/main/java/org/example/swaggerfiletest/DTO/HelloDTO.java
+++ SwaggerFileTest/src/main/java/org/example/swaggerfiletest/DTO/HelloDTO.java
@@ -4,11 +4,11 @@ import org.springframework.web.multipart.MultipartFile;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 public class HelloDTO {
 	private String hello;
-	private MultipartFile Image;
-
+	private MultipartFile image;
 }
--- SwaggerFileTest/src/main/java/org/example/swaggerfiletest/FileController.java
+++ SwaggerFileTest/src/main/java/org/example/swaggerfiletest/FileController.java
@@ -2,6 +2,7 @@ package org.example.swaggerfiletest;
 
 import org.example.swaggerfiletest.DTO.HelloDTO;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api/file")
 public class FileController {
 
-	@PostMapping(value = "/file", consumes = {"multipart/form-data"})
+	// @PostMapping(value = "/file/requestParam", consumes = {"multipart/form-data"})
 	public ResponseEntity<?> fileUpload(@RequestParam("hello") String hello, @RequestParam("image") MultipartFile image) {
 		HelloDTO helloDTO = new HelloDTO(hello, image);
 
@@ -23,4 +24,16 @@ public class FileController {
 
 		return ResponseEntity.ok(message);
 	}
+
+	@PostMapping(value = "/modelAttribute", consumes = "multipart/form-data")
+	public ResponseEntity<?> fileUpload2(@ModelAttribute HelloDTO helloDTO) {
+
+		String message = String.format("Hello: %s, File Name: %s, File Size: %d bytes",
+			helloDTO.getHello(),
+			helloDTO.getImage().getOriginalFilename(),
+			helloDTO.getImage().getSize());
+
+		return ResponseEntity.ok(message);
+	}
+
 }
