b005ef16e27c75b18f3ebf2fe6fef16d05d90e63
YellTa <>
2025-01-14 13:16:15 +0900

    first commit

---
+++ SwaggerFileTest/src/main/java/org/example/swaggerfiletest/SwaggerFileTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.swaggerfiletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerFileTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerFileTestApplication.class, args);
+	}
+
+}
