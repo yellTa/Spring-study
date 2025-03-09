a78cb8285ba8ca467ab36352e6344d4beca55a4f
micka <bbubboru22@gmail.com>
2025-03-09 23:35:48 +0900

    first commit

---
+++ vault-setup/vaultSetup/src/main/groovy/example/vaultsetup/VaultSetupApplication.groovy
@@ -0,0 +1,13 @@
+package example.vaultsetup
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class VaultSetupApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(VaultSetupApplication, args)
+    }
+
+}
---
+++ vault-setup/vaultSetup/src/test/groovy/example/vaultsetup/VaultSetupApplicationTests.groovy
@@ -0,0 +1,13 @@
+package example.vaultsetup
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VaultSetupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
