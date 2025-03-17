e4e0862e53f7b5457f51ca6edc4e5089c5f0b175
micka <bbubboru22@gmail.com>
2025-03-17 20:59:48 +0900

    BestAPIRecommendation first commit

---
+++ BestAPIRecommendation/AggregationBestContents/src/main/java/example/aggregationbestcontents/AggregationBestContentsApplication.java
@@ -0,0 +1,13 @@
+package example.aggregationbestcontents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AggregationBestContentsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AggregationBestContentsApplication.class, args);
+  }
+
+}
