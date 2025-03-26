81f6f5671bcd9540624dec97ad53377516efe271
micka <bbubboru22@gmail.com>
2025-03-26 17:05:36 +0900

    build : querydsl, 배치 작업 셋업 AggregationBestContentsApplication.java==== 배치 작업 수행을 위한  @EnableScheduling애노테이션 추가
    build.gradle ====
    querydsl의존성 추가
    
    ExcessDeathUpdateRequest.java=====
    저장시 들어오는 데이터 형식지정
    
    config.java====
    querydsl 종속성 추가

--- BestAPIRecommendation/AggregationBestContents/src/main/java/example/aggregationbestcontents/AggregationBestContentsApplication.java
+++ BestAPIRecommendation/AggregationBestContents/src/main/java/example/aggregationbestcontents/AggregationBestContentsApplication.java
@@ -1,9 +1,12 @@
 package example.aggregationbestcontents;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class AggregationBestContentsApplication {
 
 	public static void main(String[] args) {
--- BestAPIRecommendation/AggregationBestContents/src/main/java/example/aggregationbestcontents/dto/ExcessDeathUpdateRequest.java
+++ BestAPIRecommendation/AggregationBestContents/src/main/java/example/aggregationbestcontents/dto/ExcessDeathUpdateRequest.java
@@ -1,4 +1,4 @@
-package example.aggregationbestcontents.DTO;
+package example.aggregationbestcontents.dto;
 
 import java.time.LocalDate;
 
